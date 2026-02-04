cb6cd2d2d8be5236b2e01ab05aac9a257f529b2c
the sun <2261487552@qq.com>
2026-02-04 08:49:02 +0800

    2026.02.03

--- Machine Learning/day05/作业1/代码.py
+++ Machine Learning/day05/作业1/代码.py
@@ -10,13 +10,13 @@ if __name__ == '__main__':
     _, mask = cv2.threshold(ice_gray, 70, 255, cv2.THRESH_BINARY_INV)
 
     rows, cols = chicken.shape[:2]
-    rol = img[10:10+rows, 370:370+cols]
+    rol = img[240:240+rows, 200:200+cols]
 
     img_and = cv2.bitwise_and(rol, rol, mask=mask)
 
     dat = cv2.add(img_and, chicken)
 
-    img[10:10+rows, 370:370+cols] = dat
+    img[240:240+rows, 200:200+cols] = dat
 
     cv2.imshow('img', img)
     cv2.imwrite('homework1.jpg', img)
